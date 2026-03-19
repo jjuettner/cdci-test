0d2d5949fd548f00166fc8bbfa5a235136b3623a
Jens Jüttner <jens.juettner1@gmail.com>
2026-03-19 02:45:21 +0100

    1

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	base: "/MyAppTest/",
+	base: "/cdci-test/",
 	server: {
 		open: true,
 		port: 3001,
